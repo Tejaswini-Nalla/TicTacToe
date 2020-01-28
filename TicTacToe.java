f300642d0d82acf7e53e1b5f85d890f15b7c5cfe
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-01-28 15:30:17 +0530

    Add some logic changes

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,6 +3,7 @@ class TicTacToe{
 	final int player2=-1;
 	public int player=1;
 	public int gameBoard[][]=new int[3][3];
+	public boolean isGameOver=false;
 	public boolean checkplace(int i,int j)
 	{ 	
 		if(gameBoard[i][j]!=0){
@@ -13,16 +14,8 @@ class TicTacToe{
 			System.out.println("Enter correct positions");
 			return false;
 		}
-		System.out.print("place available");
+		//System.out.print("place available");
 		gameBoard[i][j]=player;
-		if(player==1)
-		{
-			player=-1;
-		}
-		else
-		{
-			player=1;
-		}
 		return true;
 	}
 	boolean checkWinner(int player)
@@ -31,6 +24,7 @@ class TicTacToe{
 		   gameBoard[0][0]+gameBoard[1][0]+gameBoard[2][0]==3*player||gameBoard[0][1]+gameBoard[1][1]+gameBoard[2][1]==3*player||gameBoard[0][2]+gameBoard[1][2]+gameBoard[2][2]==3*player||
 		   gameBoard[0][0]+gameBoard[1][1]+gameBoard[2][2]==3*player||gameBoard[1][1]+gameBoard[0][2]+gameBoard[2][0]==3*player)
 		{
+			isGameOver=true;
 			return true;
 		}
 		return false;
--- tictac.java
+++ tictac.java
@@ -1,36 +1,41 @@
 import java.util.Scanner;
 class tictac{
-	static boolean isGameOver=false;
-	static int i,j;
+	static int row,col;
       
 	
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
 		TicTacToe game=new TicTacToe();
-		while(!isGameOver)
+		while(!game.isGameOver)
 		{
 			System.out.println(game.player==game.player1?"Player1 choice":"Player2 choice");
-			i=s.nextInt();
-			j=s.nextInt();
-			if(game.checkplace(i,j))
+			row=s.nextInt();
+			col=s.nextInt();
+			if(game.checkplace(row,col))
 			{
 				if(game.checkWinner(game.player))
 				{  
+
 					if(game.player==game.player1)
 					{
-						System.out.println(isGameOver);
 						System.out.println("Player1 wins");
-						isGameOver=true;
-						System.out.println("after chage"+isGameOver);
+						break;
 					}
 					else
 					{
 						System.out.println("Player2 wins");
-						isGameOver=true;
 					}
 						
 				}
 			}
+			if(game.player==game.player1)
+		   {
+			   game.player=game.player2;
+		   }
+		  else
+		   {
+			   game.player=game.player1;
+		   }
 			
 		}
 		
