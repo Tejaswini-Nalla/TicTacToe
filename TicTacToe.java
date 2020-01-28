34d4d6cc0195744ecc6ca49dd65f3ada52c5112c
Tejaswini-Nalla <tejaswini.nalla@thoughtworks.com>
2020-01-28 14:37:22 +0530

    Add Class TicTacToe

---
+++ TicTacToe.java
@@ -0,0 +1,39 @@
+class TicTacToe{
+	final int player1=1;
+	final int player2=-1;
+	public int player=1;
+	public int gameBoard[][]=new int[3][3];
+	public boolean checkplace(int i,int j)
+	{ 	
+		if(gameBoard[i][j]!=0){
+			System.out.println("Already placed");
+			return false;
+		}
+		if(i<0 || i>2 || j<0 || j>2){
+			System.out.println("Enter correct positions");
+			return false;
+		}
+		
+		gameBoard[i][j]=player;
+		if(player==1)
+		{
+			player=-1;
+		}
+		else
+		{
+			player=1;
+		}
+		return true;
+	}
+	boolean checkWinner(int player)
+	{
+		if(gameBoard[0][0]+gameBoard[0][1]+gameBoard[0][2]==3*player||gameBoard[1][0]+gameBoard[1][1]+gameBoard[1][2]==3*player||gameBoard[2][0]+gameBoard[2][1]+gameBoard[2][2]==3*player||
+		   gameBoard[0][0]+gameBoard[1][0]+gameBoard[2][0]==3*player||gameBoard[0][1]+gameBoard[1][1]+gameBoard[2][1]==3*player||gameBoard[0][2]+gameBoard[1][2]+gameBoard[2][2]==3*player||
+		   gameBoard[0][0]+gameBoard[1][1]+gameBoard[2][2]==3*player||gameBoard[1][1]+gameBoard[0][2]+gameBoard[2][0]==3*player)
+		{
+			return true;
+		}
+		return false;
+	}
+        
+}
--- tictac.java
+++ tictac.java
@@ -1,72 +1,28 @@
 import java.util.Scanner;
 class tictac{
-	static String user1_name;
-	static String user2_name;
-	static int user1_choice;
-	static int user2_choice;
-        static int player=1;
 	static boolean isGameOver=false;
-	static int gameBoard[][]=new int[3][3];
 	static int i,j;
-        static boolean checkplace(int i,int j)
-	{ 	
-		if(gameBoard[i][j]!=0){
-			System.out.println("Already placed");
-			return false;
-		}
-		if(i<0 || i>2 || j<0 || j>2){
-			System.out.println("Enter correct positions");
-			return false;
-		}
-		
-		gameBoard[i][j]=player;
-		if(player==1)
-		{
-			player=-1;
-		}
-		else
-		{
-			player=1;
-		}
-		return true;
-	}
-	static boolean checkWinner(int player)
-	{
-		if(gameBoard[0][0]+gameBoard[0][1]+gameBoard[0][2]==3*player||gameBoard[1][0]+gameBoard[1][1]+gameBoard[1][2]==3*player||gameBoard[2][0]+gameBoard[2][1]+gameBoard[2][2]==3*player||
-		   gameBoard[0][0]+gameBoard[1][0]+gameBoard[2][0]==3*player||gameBoard[0][1]+gameBoard[1][1]+gameBoard[2][1]==3*player||gameBoard[0][2]+gameBoard[1][2]+gameBoard[2][2]==3*player||
-		   gameBoard[0][0]+gameBoard[1][1]+gameBoard[2][2]==3*player||gameBoard[1][1]+gameBoard[0][2]+gameBoard[2][0]==3*player)
-		{
-			isGameOver=true;
-			return true;
-		}
-		return false;
-	}
+      
+	
 	public static void main(String[] args){
-		Scanner s=new Scanner(System.in);
-		System.out.println("Enter the user1 name");
-		user1_name=s.next();
-		System.out.println("Enter user1 choice '-1' or '1'");
-		user1_choice=s.nextInt();
-		System.out.println("Enter the user2 name");
-                user2_name=s.next();
-                System.out.println("Enter user2 choice '-1' or '1'");
-                user2_choice=s.nextInt();
+		Scanner s = new Scanner(System.in);
+		TicTacToe game=new TicTacToe();
 		while(!isGameOver)
 		{
-			System.out.println(player==1?"User 1 choice":"User 2 choice");
+			System.out.println(game.player==game.player1?"Player1 1 choice":"Player2 choice");
 			i=s.nextInt();
 			j=s.nextInt();
-			if(checkplace(i,j))
+			if(game.checkplace(i,j))
 			{
-				if(checkWinner(player))
+				if(game.checkWinner(game.player))
 				{  
-					if(player==1)
+					if(game.player==game.player1)
 					{
-						System.out.println("User1 wins");
+						System.out.println("Player1 wins");
 					}
 					else
 					{
-						System.out.println("User2 wins");
+						System.out.println("Player2 wins");
 					}
 						
 				}
